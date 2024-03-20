a0fee7d22f4f20a65f4877f59a3fa540d8b5b18d
Meghanath Ms <meghanathms123@gmail.com>
2024-03-20 21:37:05 +0530

    day2

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import NavBar from './components/navbar/NavBar'
+import {Route,Routes} from "react-router-dom"
+import Cart from './pages/cart/Cart'
+import Home from './pages/home/Home'
+import PlaceOrder from './pages/placeorder/PlaceOrder'
+
 
 const App = () => {
   return (
     <div className='App'>
       <NavBar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/order" element={<PlaceOrder/>} />
+       
+      </Routes>
     </div>
   )
 }
